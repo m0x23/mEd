f84630760326b9f80c575e05bfbad6322d93744a
m0x23 <junkee@live.de>
2015-11-15 12:11:09 +0100

    added usage

--- ed/src/ed/YesNoDialog.java
+++ ed/src/ed/YesNoDialog.java
@@ -2,16 +2,25 @@ package ed;
 
 import javax.swing.JOptionPane;
 
-public class YesNoDialog 
+public class YesNoDialog
 {
 
 	public static void main(String[] args)
 	{
 	}
+
 	public static int create(String content, String title)
 	{
-		int returnvar = JOptionPane.showConfirmDialog(null, content, title, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		int returnvar;
+		// open the dialog
+		returnvar = JOptionPane.showConfirmDialog(null, content, title, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 		return returnvar;
+		
+		// usage:
+		// if YesNoDialog.create() == 
+		// JOptionPane.NO_OPTION
+		// JOptionPane.YES_OPTION
+		// JOptionPane.CLOSED_OPTION
 	}
 
 }
